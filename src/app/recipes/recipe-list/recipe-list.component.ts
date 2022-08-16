8d374a49b7c59ac8b21e48f335a42407cd33412a
dgarcmat <deadstandark@gmail.com>
2022-08-16 12:21:22 +0200

    54,55

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is a Test','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg?20170213105318'),
+    new Recipe('A test recipe 2','This is a Test 2','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg?20170213105318'),
+    new Recipe('A test recipe 3','This is a Test 3','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg?20170213105318'),
+    new Recipe('A test recipe 4','This is a Test 4','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg?20170213105318')
+  ];
 
   constructor() { }
 
